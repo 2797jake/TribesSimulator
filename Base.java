bb8e718e50c1b511da7646396a7142a6103e882b
2797jake <jake.oakes27@gmail.com>
2014-10-02 09:48:57 -0500

    First upload
    no code just ideas

---
+++ Base.java
@@ -0,0 +1,23 @@
+/*
+Tribe Simulator
+Features:
+population
+food
+wood
+stone
+copper
+iron
+happiness
+enemy tribes and combat
+day/night cycle
+seasons
+nomadic(area slowly is depleted of resources and you have to move, leaving behind some supplies and some of the population)
+farming(no longer nomadic)
+*/
+
+public class Base
+{
+
+
+
+}
